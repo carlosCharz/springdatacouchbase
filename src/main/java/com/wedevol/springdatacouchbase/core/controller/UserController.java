f90fd2c474ce95fd5fe17b1ee4030de19edb7857
Carlos Becerra <carlos.becerra1234@gmail.com>
2017-05-06 18:05:31 -0300

    user controller

--- src/main/java/com/wedevol/springdatacouchbase/core/controller/UserController.java
+++ src/main/java/com/wedevol/springdatacouchbase/core/controller/UserController.java
@@ -1,25 +1,24 @@
 package com.wedevol.springdatacouchbase.core.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import javax.annotation.PreDestroy;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wedevol.springdatacouchbase.core.server.CcsConnection;
-import com.wedevol.springdatacouchbase.core.server.bean.CcsOutMessage;
-import com.wedevol.springdatacouchbase.core.server.mapper.MessageMapper;
-import com.wedevol.springdatacouchbase.core.util.Util;
+import com.wedevol.springdatacouchbase.core.bean.User;
+import com.wedevol.springdatacouchbase.core.service.UserService;
 
 /**
  * User REST Controller
@@ -28,43 +27,55 @@ import com.wedevol.springdatacouchbase.core.util.Util;
  *
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
 	protected static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
-	private CcsConnection ccsConnection;
+	private UserService userService;
 
-	@RequestMapping(value = "/tokendevice/{token}", method = RequestMethod.GET)
+	@RequestMapping(value = "", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
-	public void testNotification(@PathVariable("token") String token) {
-		logger.info("Controller -> test notification");
-
-		// Create test message
-		final String message = "Hello from Notifier! This is a test message!";
-		Map<String, Object> jsonMap = new HashMap<String, Object>();
-		jsonMap.put("message", message);
-		final String jsonContent = MessageMapper.toJsonString(jsonMap);
-		Map<String, String> dataPayload = new HashMap<String, String>();
-		dataPayload.put("action", "MESSAGE");
-		dataPayload.put("senderId", "1");
-		dataPayload.put("receiverId", "1");
-		dataPayload.put("type", "CHAT");
-		dataPayload.put("content", jsonContent);
-		Map<String, String> notificationPayload = new HashMap<String, String>();
-		notificationPayload.put("body", message);
-		notificationPayload.put("badge", "1");
-		notificationPayload.put("click_action", "CHAT");
-		notificationPayload.put("sound", "default");
-		final String messageId = Util.getUniqueMessageId();
-		final CcsOutMessage outMessage = new CcsOutMessage(token, messageId, 259200, dataPayload, notificationPayload,
-				"high");
-
-		// Create json request
-		final String jsonRequest = MessageMapper.toJsonString(outMessage);
-		ccsConnection.sendPacket(jsonRequest);
+	@ResponseBody
+	public List<User> findAll() {
+		return userService.findAll();
 	}
 
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public User findById(@PathVariable Long userId) {
+		return userService.findById(userId);
+	}
+	
+	@RequestMapping(value = "/find", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public List<User> findUsersByEmail(@RequestParam(value = "email") String email) {
+		logger.info("Find users by email: " + email);
+		return userService.findUsersByEmail(email);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	@ResponseBody
+	public User create(@Valid @RequestBody User User) {
+		return userService.create(user);
+	}
+
+	@RequestMapping(value = "/{userId}", method = RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public void update(@PathVariable Long userId, @Valid @RequestBody User User) {
+		userService.update(userId, User);
+	}
+
+	@RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public void delete(@PathVariable Long userId) {
+		userService.delete(userId);
+	}
 
 }

a6bf91a038e554b1fce01a3e41ef9d9b6bdd259b
Carlos Becerra <carlos.becerra1234@gmail.com>
2017-07-06 17:33:23 -0300

    controller test base

---
+++ src/test/java/com/wedevol/springdatacouchbase/core/controller/UserControllerTest.java
@@ -0,0 +1,27 @@
+package com.wedevol.springdatacouchbase.core.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Test the User Controller
+ *
+ * @author Charz++
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserControllerTest {
+	
+	private static final Long USER_ONE_ID = 1L;
+
+	@Before
+	public void init() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+}

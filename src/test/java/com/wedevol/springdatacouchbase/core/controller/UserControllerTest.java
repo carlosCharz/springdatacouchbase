defca87defa12d133634653e32a130bf9ee91e99
Carlos Becerra <carlos.becerra1234@gmail.com>
2017-07-06 19:35:08 -0300

    first draft

--- src/test/java/com/wedevol/springdatacouchbase/core/controller/UserControllerTest.java
+++ src/test/java/com/wedevol/springdatacouchbase/core/controller/UserControllerTest.java
@@ -1,9 +1,36 @@
 package com.wedevol.springdatacouchbase.core.controller;
 
-import org.junit.After;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.wedevol.springdatacouchbase.core.EntryPoint;
+import com.wedevol.springdatacouchbase.core.dao.UserRepository;
+import com.wedevol.springdatacouchbase.core.dao.doc.UserDoc;
 
 /**
  * Test the User Controller
@@ -11,17 +38,61 @@ import org.mockito.runners.MockitoJUnitRunner;
  * @author Charz++
  */
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EntryPoint.class)
+@WebAppConfiguration
 public class UserControllerTest {
-	
+
 	private static final Long USER_ONE_ID = 1L;
+
+	private static final String USER_ONE_KEY = UserDoc.getKeyFor(USER_ONE_ID);
+
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	private MockMvc mockMvc;
+	private HttpMessageConverter mappingJackson2HttpMessageConverter;
+	private UserDoc user;
+	private List<UserDoc> userList = new ArrayList<>();
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Autowired
+	public void setConverters(HttpMessageConverter<?>[] converters) {
+		this.mappingJackson2HttpMessageConverter = Arrays.asList(converters)
+															.stream()
+															.filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+															.findAny()
+															.orElse(null);
+		assertNotNull("the JSON message converter must not be null", this.mappingJackson2HttpMessageConverter);
+	}
 
 	@Before
 	public void init() {
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+		//this.userRepository.deleteAllInBatch();
+
+		this.user = userRepository.save(new UserDoc(USER_ONE_KEY, USER_ONE_ID, "Carlos1", null, 26, "carlos1@yopmail.com"));
 	}
 
-	@After
-	public void tearDown() {
+    @Test
+    public void readSingleUser() throws Exception {
+        mockMvc.perform(get("/users/" + USER_ONE_ID))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.id", is("1")))
+                .andExpect(jsonPath("$.name", is("Carlos")))
+                .andExpect(jsonPath("$.email", is("A description")));
+    }
+
+	protected String json(Object o) throws IOException {
+		MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+		this.mappingJackson2HttpMessageConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+		return mockHttpOutputMessage.getBodyAsString();
 	}
 
 }

60b1c04bba99ebe0e1b8ddac0289d93bb926b41c
Carlos Becerra <carlos.becerra1234@gmail.com>
2017-07-09 17:50:36 -0300

    code refactor + comments

--- src/test/java/com/wedevol/springdatacouchbase/core/controller/UserControllerTest.java
+++ src/test/java/com/wedevol/springdatacouchbase/core/controller/UserControllerTest.java
@@ -1,8 +1,5 @@
 package com.wedevol.springdatacouchbase.core.controller;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -12,11 +9,11 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
+import org.hamcrest.Matchers;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -51,23 +48,21 @@ public class UserControllerTest {
 	private static final Long USER_TWO_ID = 2L;
 	private static final Long USER_THREE_ID = 3L;
 	private static final Long USER_FOUR_ID = 4L;
-
 	private static final String USER_ONE_KEY = UserDoc.getKeyFor(USER_ONE_ID);
 	private static final String USER_TWO_KEY = UserDoc.getKeyFor(USER_TWO_ID);
 	private static final String USER_THREE_KEY = UserDoc.getKeyFor(USER_THREE_ID);
 	private static final String USER_FOUR_KEY = UserDoc.getKeyFor(USER_FOUR_ID);
-
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	private static MediaType CONTENT_TYPE = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	
 	private MockMvc mockMvc;
+	@SuppressWarnings("rawtypes")
 	private HttpMessageConverter mappingJackson2HttpMessageConverter;
-	private UserDoc user;
-	private List<UserDoc> userList = new ArrayList<>();
-
-	@Autowired
-	private UserRepository userRepository;
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@Autowired
 	public void setConverters(HttpMessageConverter<?>[] converters) {
@@ -76,25 +71,25 @@ public class UserControllerTest {
 															.filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
 															.findAny()
 															.orElse(null);
-		assertNotNull("the JSON message converter must not be null", this.mappingJackson2HttpMessageConverter);
+		Assert.assertNotNull("the JSON message converter must not be null", this.mappingJackson2HttpMessageConverter);
 	}
 
 	@Before
 	public void init() {
 		mockMvc = webAppContextSetup(webApplicationContext).build();
-		user = new UserDoc(USER_ONE_KEY, USER_ONE_ID, "Carlos", Arrays.asList("charz"), 26, "carlos@yopmail.com");
-		UserDoc user2 = new UserDoc(USER_TWO_KEY, USER_TWO_ID, "Carlos2", Arrays.asList("charz", "charles2"), 25, "carlos2@yopmail.com");
-		UserDoc user3 = new UserDoc(USER_THREE_KEY, USER_THREE_ID, "Carlos3", Arrays.asList("charz", "charles3"), 26, "carlos3@yopmail.com");
-		
-		userRepository.save(user);
+		// create the users
+		UserDoc user1 = new UserDoc(USER_ONE_KEY, USER_ONE_ID, "Carlos", Arrays.asList("charz"), 26, "carlos@yopmail.com");
+		UserDoc user2 = new UserDoc(USER_TWO_KEY, USER_TWO_ID, "Luis", Arrays.asList("charz", "lucho"), 25, "luis@yopmail.com");
+		UserDoc user3 = new UserDoc(USER_THREE_KEY, USER_THREE_ID, "Jose", Arrays.asList("pepe"), 26, "jose@yopmail.com");
+		// save the users in the database
+		userRepository.save(user1);
 		userRepository.save(user2);
 		userRepository.save(user3);
-		
-		userList = Arrays.asList(user, user2, user3);
 	}
 	
 	@After
 	public void tearDown() {
+		// delete all the test data created from the database
 		userRepository.delete(USER_ONE_KEY);
 		userRepository.delete(USER_TWO_KEY);
 		userRepository.delete(USER_THREE_KEY);
@@ -106,26 +101,26 @@ public class UserControllerTest {
     public void getNonExistingUser() throws Exception {
         mockMvc.perform(get("/users/100"))
                 .andExpect(status().isNotFound())
-                .andExpect(content().contentType(contentType));
+                .andExpect(content().contentType(CONTENT_TYPE));
     }
 
     @Test
     public void getExistingUser() throws Exception {
         mockMvc.perform(get("/users/" + USER_ONE_ID))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$.id", is(1)))
-                .andExpect(jsonPath("$.nicknames[0]", is("charz")))
-                .andExpect(jsonPath("$.name", is("Carlos")))
-                .andExpect(jsonPath("$.age", is(26)))
-                .andExpect(jsonPath("$.email", is("carlos@yopmail.com")));
+                .andExpect(content().contentType(CONTENT_TYPE))
+                .andExpect(jsonPath("$.id", Matchers.is(1)))
+                .andExpect(jsonPath("$.nicknames[0]", Matchers.is("charz")))
+                .andExpect(jsonPath("$.name", Matchers.is("Carlos")))
+                .andExpect(jsonPath("$.age", Matchers.is(26)))
+                .andExpect(jsonPath("$.email", Matchers.is("carlos@yopmail.com")));
     }
     
     @Test
     public void createUser() throws Exception {
-    	UserDoc user4 = new UserDoc(USER_FOUR_KEY, USER_FOUR_ID, "Carlos4", Arrays.asList("charles4"), 30, "carlos4@yopmail.com");
+    	UserDoc user4 = new UserDoc(USER_FOUR_KEY, USER_FOUR_ID, "Alfredo", Arrays.asList("alfredo"), 30, "alfredo@yopmail.com");
         mockMvc.perform(post("/users")
-               .contentType(contentType)
+               .contentType(CONTENT_TYPE)
                .content(json(user4)))
                .andExpect(status().isCreated());
     }
@@ -134,18 +129,18 @@ public class UserControllerTest {
     public void existUser() throws Exception {
         mockMvc.perform(get("/users/" + USER_ONE_ID + "/exists"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$", is(true)));
+                .andExpect(content().contentType(CONTENT_TYPE))
+                .andExpect(jsonPath("$", Matchers.is(true)));
     }
     
     @Test
     public void getExistingUserByEmail() throws Exception {
         mockMvc.perform(get("/users/find/email")
-        			.param("email", "carlos2@yopmail.com"))
+        			.param("email", "luis@yopmail.com"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$.id", is(2)))
-                .andExpect(jsonPath("$.email", is("carlos2@yopmail.com")));
+                .andExpect(content().contentType(CONTENT_TYPE))
+                .andExpect(jsonPath("$.id", Matchers.is(2)))
+                .andExpect(jsonPath("$.email", Matchers.is("luis@yopmail.com")));
     }
     
     @Test
@@ -153,8 +148,8 @@ public class UserControllerTest {
         mockMvc.perform(get("/users/find/nickname")
         			.param("nickname", "charz"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$", hasSize(3)));
+                .andExpect(content().contentType(CONTENT_TYPE))
+                .andExpect(jsonPath("$", Matchers.hasSize(2)));
     }
 
 	@SuppressWarnings("unchecked")

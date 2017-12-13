551e36a7757a6c567e4c90154a0666d921cd0b39
Carlos Becerra <carlos.becerra1234@gmail.com>
2017-12-13 11:18:58 -0500

    hashCode and equals fix

--- src/main/java/com/wedevol/springdatacouchbase/core/dao/doc/UserBasicDoc.java
+++ src/main/java/com/wedevol/springdatacouchbase/core/dao/doc/UserBasicDoc.java
@@ -48,24 +48,33 @@ public class UserBasicDoc implements Serializable {
 
 	@Override
 	public int hashCode() {
-		int hashed = 1;
-		if (id != null) {
-			hashed = hashed * 31 + id.hashCode();
-		}
-		if (name != null) {
-			hashed = hashed * 31 + name.hashCode();
-		}
-		return hashed;
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj == null || obj.getClass() != this.getClass())
-			return false;
-		if (obj == this)
+		if (this == obj)
 			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
 		UserBasicDoc other = (UserBasicDoc) obj;
-		return this.hashCode() == other.hashCode();
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 }
--- src/main/java/com/wedevol/springdatacouchbase/core/dao/doc/UserDoc.java
+++ src/main/java/com/wedevol/springdatacouchbase/core/dao/doc/UserDoc.java
@@ -107,33 +107,57 @@ public class UserDoc implements Serializable {
 
 	@Override
 	public int hashCode() {
-		int hashed = 1;
-		if (id != null) {
-			hashed = hashed * 31 + id.hashCode();
-		}
-		if (name != null) {
-			hashed = hashed * 31 + name.hashCode();
-		}
-		if (nicknames != null) {
-			hashed = hashed * 31 + nicknames.hashCode();
-		}
-		if (age != null) {
-			hashed = hashed * 31 + age.hashCode();
-		}
-		if (email != null) {
-			hashed = hashed * 31 + email.hashCode();
-		}
-		return hashed;
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((nicknames == null) ? 0 : nicknames.hashCode());
+		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj == null || obj.getClass() != this.getClass())
-			return false;
-		if (obj == this)
+		if (this == obj)
 			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
 		UserDoc other = (UserDoc) obj;
-		return this.hashCode() == other.hashCode();
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (nicknames == null) {
+			if (other.nicknames != null)
+				return false;
+		} else if (!nicknames.equals(other.nicknames))
+			return false;
+		return true;
 	}
 
 }

56b136cbcc42db8ef30b45c3bf64990cd4830e21
Carlos Becerra <carlos.becerra1234@gmail.com>
2020-04-18 19:57:33 -0500

    apply format and cleanup

--- src/main/java/com/wedevol/springdatacouchbase/core/exception/ErrorType.java
+++ src/main/java/com/wedevol/springdatacouchbase/core/exception/ErrorType.java
@@ -6,9 +6,12 @@ package com.wedevol.springdatacouchbase.core.exception;
  * @author charz
  */
 public enum ErrorType {
-  USER_NOT_FOUND(1, "User not found"), USER_ALREADY_EXISTS(2, "User already exists"), CAR_NOT_FOUND(3,
-      "Car not found"), PRODUCT_NOT_FOUND(4,
-          "Product not found"), PLACE_NOT_FOUND(5, "Place not found"), PHONE_NOT_FOUND(6, "Phone not found");
+  USER_NOT_FOUND(1, "User not found"),
+  USER_ALREADY_EXISTS(2, "User already exists"),
+  CAR_NOT_FOUND(3, "Car not found"),
+  PRODUCT_NOT_FOUND(4, "Product not found"),
+  PLACE_NOT_FOUND(5, "Place not found"),
+  PHONE_NOT_FOUND(6, "Phone not found");
 
   private final int code;
   private final String message;

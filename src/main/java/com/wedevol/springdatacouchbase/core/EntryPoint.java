6d564651562e5504f1013d6137c02b5bf96cdaf0
Carlos Becerra <carlos.becerra1234@gmail.com>
2020-05-13 21:51:25 -0500

    remove unused extend in entry point

--- src/main/java/com/wedevol/springdatacouchbase/core/EntryPoint.java
+++ src/main/java/com/wedevol/springdatacouchbase/core/EntryPoint.java
@@ -1,10 +1,7 @@
 package com.wedevol.springdatacouchbase.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Entry Point class
@@ -15,12 +12,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 
 @SpringBootApplication
-public class EntryPoint extends SpringBootServletInitializer {
-
-    private static final Logger logger = LoggerFactory.getLogger(EntryPoint.class);
+public class EntryPoint {
 
     public static void main(String[] args) {
-        logger.info("Initializing Spring Boot!");
         SpringApplication.run(EntryPoint.class, args);
     }
 
